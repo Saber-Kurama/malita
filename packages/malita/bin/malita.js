596d5db755baefcaaca66101225d924722b7dbe1
guochao <463561248@qq.com>
2022-09-16 14:28:31 +0800

    ✨ feat:  day5

--- packages/malita/bin/malita.js
+++ packages/malita/bin/malita.js
@@ -1,23 +1,21 @@
 #!/usr/bin/env node
 
-const {
-    Command
-} = require('commander');
-
+const { Command } = require("commander");
 
 const program = new Command();
 
 program
-    .version(require('../package.json').version, '-v, -V', '输出当前框架的版本')
-    .description('这是21天短文，挑战手写前端框架的产物框架')
-    .usage('<command> [options]')
-    .parse(process.argv);
+  .version(require("../package.json").version, "-v, -V", "输出当前框架的版本")
+  .description("这是21天短文，挑战手写前端框架的产物框架")
+  .usage("<command> [options]")
+  .parse(process.argv);
 
-program.command('help')
-    .alias('-h')
-    .description('帮助命令')
-    .action(function(name, other) {
-        console.log(`
+program
+  .command("help")
+  .alias("-h")
+  .description("帮助命令")
+  .action(function (name, other) {
+    console.log(`
 这是21天短文，挑战手写前端框架的产物框架 malita
 
 支持的命令:
@@ -25,5 +23,15 @@ program.command('help')
   help,-h 输出帮助程序
 
 Example call:
-    $ malita <command> --help`)
-    }).parse(process.argv);
+    $ malita <command> --help`);
+  })
+  .parse(process.argv);
+
+program
+  .command("dev")
+  .description("框架开发命令")
+  .action(function () {
+    require("../lib/dev");
+  });
+
+program.parse(process.argv);
